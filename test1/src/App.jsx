febe3db22d77b1a175982bfc023833a6e339dce1
rohit73199 <727722euit150@skcet.ac.in>
2023-11-08 14:34:30 +0530

    u

--- test1/src/App.jsx
+++ test1/src/App.jsx
@@ -1,8 +1,11 @@
-import './App.css';
-import Buttons from './components/Buttons';
+{/*import './App.css';*/}
 {/*import Navbar from './components/Navbar';
 import Main from './components/Main';
+import Buttons from './components/Buttons';
 import Footer from './components/Footer';*/}
+import DynamicRendering from './day-3/DynamicRendering';
+import Counter from './day-3/Counter';
+import ToggleMessage from './day-3/ToggleMessage';
 
 function App() {
   return(
@@ -11,12 +14,15 @@ function App() {
 
   {/*<Navbar></Navbar>
   <Main></Main>
-  <Footer></Footer>*/}
+  <Footer></Footer>
       <Buttons label="Green button" color="green" />
       <Buttons label="Yellow button" color="blue"/>
       <Buttons label="Black button" color="white"/>
-      <Buttons label="red button" color="red"/>
-    </div>
+  <Buttons label="red button" color="red"/>*/}
+  <DynamicRendering/>
+  <Counter/>
+  <ToggleMessage/>
+  </div>
   );
 }
 
